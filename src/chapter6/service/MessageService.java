bfa6bad001acd193b5962f6d0982471dd5213b1d
Daiki_Yamane <yamane.daiki@alhinc.jp>
2025-01-29 17:08:57 +0900

    簡易Twitter課題コードレビュー依頼2回目

--- src/chapter6/service/MessageService.java
+++ src/chapter6/service/MessageService.java
@@ -8,6 +8,8 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.commons.lang.StringUtils;
+
 import chapter6.beans.Message;
 import chapter6.beans.UserMessage;
 import chapter6.dao.MessageDao;
@@ -33,7 +35,7 @@ public class MessageService {
 	public void insert(Message message) {
 
 		log.info(new Object(){}.getClass().getEnclosingClass().getName() +
-		" : " + new Object(){}.getClass().getEnclosingMethod().getName());
+				" : " + new Object(){}.getClass().getEnclosingMethod().getName());
 
 		Connection connection = null;
 		try {
@@ -53,17 +55,34 @@ public class MessageService {
 		}
 	}
 
-	public List<UserMessage> select() {
+	public List<UserMessage> select(String userId) {
 
 		log.info(new Object(){}.getClass().getEnclosingClass().getName() +
-		" : " + new Object(){}.getClass().getEnclosingMethod().getName());
+				" : " + new Object(){}.getClass().getEnclosingMethod().getName());
 
 		final int LIMIT_NUM = 1000;
 
 		Connection connection = null;
 		try {
 			connection = getConnection();
-			List<UserMessage> messages = new UserMessageDao().select(connection, LIMIT_NUM);
+
+			/*
+			 * idをnullで初期化
+			 * ServletからuserIdの値が渡ってきていたら
+			 * 整数型に型変換し、idに代入
+			 */
+			Integer id = null;
+			if(!StringUtils.isEmpty(userId)) {
+				id = Integer.parseInt(userId);
+			}
+
+			/*
+			 * messageDao.selectに引数としてInteger型のidを追加
+			 * idがnullだったら全件取得する
+			 * idがnull以外だったら、その値に対応するユーザーIDの投稿を取得する
+			 */
+
+			List<UserMessage> messages = new UserMessageDao().select(connection, id, LIMIT_NUM);
 			commit(connection);
 
 			return messages;
